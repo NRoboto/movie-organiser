8fcdd72b1a819fdd1ab28e40a42f72d1da26142d
Mike <mnightingale@tutanota.com>
2020-10-08 16:00:07 +0100

    Refactor DB initialization

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
-mongoose
-  .connect(process.env.MONGODB_URL!, {
+export const initMongoose = async () => {
+  await mongoose.connect(process.env.MONGODB_URL!, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .catch((e) => console.log(chalk.bgRed("Error"), e));
+  });
 
-mongoose.connection.on("error", (err) => {
-  console.log(chalk.bgRed("Error"), err);
-});
+  mongoose.connection.on("error", (err) => {
+    console.log(chalk.bgRed("Error"), err);
+  });
+
+  return mongoose.STATES[mongoose.connection.readyState];
+};
