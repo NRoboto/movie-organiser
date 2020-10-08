967d0cc1b75c4b8ab09d204a1dea3c94ac780411
Mike <mnightingale@tutanota.com>
2020-10-08 16:01:05 +0100

    Add mongoose init to index

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
 const chalk_1 = __importDefault(require("chalk"));
+const init_1 = require("./db/init");
 const app = express_1.default();
 app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+init_1.initMongoose()
+    .then((status) => {
+    console.log("MongoDB", chalk_1.default.bgGreen(status));
+})
+    .catch((err) => {
+    console.log(chalk_1.default.bgRed("Error"), err);
+});
 app.listen(process.env.PORT, () => {
     console.log(chalk_1.default.bgMagenta(`Server is listening on port ${process.env.PORT}`));
 });
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,18 @@ import path from "path";
 import express from "express";
 import chalk from "chalk";
 
+import { initMongoose } from "./db/init";
+
 const app = express();
 app.use(express.static(path.join(__dirname, "../public")));
+
+initMongoose()
+  .then((status) => {
+    console.log("MongoDB", chalk.bgGreen(status));
+  })
+  .catch((err) => {
+    console.log(chalk.bgRed("Error"), err);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(
