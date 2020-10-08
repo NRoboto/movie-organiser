91a93903ff9bbb8ea37b0c1165dcd6bb80b66194
Mike <mnightingale@tutanota.com>
2020-10-08 16:00:27 +0100

    Add DB initialization test

---
+++ src/db/init.test.ts
@@ -0,0 +1,8 @@
+import request from "supertest";
+import mongoose from "mongoose";
+
+test("Should connect to database", async () => {
+  const { initMongoose } = await import("./init");
+  await initMongoose();
+  expect(mongoose.connection.readyState).toBe(1); // 1 = Connected
+});
