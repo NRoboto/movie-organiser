eec70b7524ae7c79166cc654b9005bfd4da1ec09
Mike <mnightingale@tutanota.com>
2020-11-05 01:17:04 +0000

    Add router

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import express from "express";
 import chalk from "chalk";
 
 import { initMongoose } from "./db/init";
+import { router } from "./router";
 
 const app = express();
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+app.use(router);
 
 initMongoose()
   .then((status) => {
