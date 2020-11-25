09fed9ea6b09226c093c1196ac950b99d4004e91
Mike <mnightingale@tutanota.com>
2020-11-25 00:11:14 +0000

    Add useAuth middleware

--- src/router.ts
+++ src/router.ts
@@ -11,9 +11,25 @@ import {
   getMovies,
 } from "./controllers";
 
+// useAuth provides req.user if a token is provided, otherwise req.user is undefined
+const useAuth: express.RequestHandler = (req, res, next) =>
+  passport.authenticate(
+    "jwt",
+    {
+      session: false,
+    },
+    (err, user, info) => {
+      if (err) return next(err);
+      if (!user) return next();
+      req.user = user;
+      next();
+    }
+  )(req, res, next);
+
 const requireAuth = passport.authenticate("jwt", {
   session: false,
 });
+
 const requireSignin = passport.authenticate("local", {
   session: false,
 });
@@ -22,7 +38,7 @@ export const router = express.Router();
 
 // Users
 router.get("/user/", searchUser, requireAuth, readSelf); // If no search query params, searchUser will pass handling to readUser
-router.get("/user/:username", readUser);
+router.get("/user/:username", useAuth, readUser);
 router.patch("/user", requireAuth, updateUser);
 router.delete("/user", requireAuth, deleteUser);
 
