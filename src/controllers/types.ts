759800939d5d987cc1e367fdb17dfda7afd0b283
Mike <mnightingale@tutanota.com>
2021-01-09 01:21:45 +0000

    Refactor request handler types to remove P as a generic

--- src/controllers/types.ts
+++ src/controllers/types.ts
@@ -4,39 +4,36 @@ import { UserDocument } from "../models/User";
 type NextFunction = (err?: { message: string; status?: number }) => any;
 
 export interface ReqAuthRequestHandler<
-  P = Record<string, string>,
   ResBody = any,
   ReqBody = any,
   ReqQuery = Record<string, any>
 > {
   (
-    req: Request<P, ResBody, ReqBody, ReqQuery>,
+    req: Request<Record<string, string>, ResBody, ReqBody, ReqQuery>,
     res: Response<ResBody>,
     next: NextFunction,
     authedUser: UserDocument
   ): any;
 }
 export interface UseAuthRequestHandler<
-  P = Record<string, string>,
   ResBody = any,
   ReqBody = any,
   ReqQuery = Record<string, any>
 > {
   (
-    req: Request<P, ResBody, ReqBody, ReqQuery>,
+    req: Request<Record<string, string>, ResBody, ReqBody, ReqQuery>,
     res: Response<ResBody>,
     next: NextFunction,
     authedUser?: UserDocument
   ): any;
 }
 export interface SigninRequestHandler<
-  P = Record<string, string>,
   ResBody = any,
   ReqBody = any,
   ReqQuery = Record<string, any>
 > {
   (
-    req: Request<P, ResBody, ReqBody, ReqQuery>,
+    req: Request<Record<string, string>, ResBody, ReqBody, ReqQuery>,
     res: Response<ResBody>,
     next: NextFunction,
     authedUser: UserDocument
