18b83d9a663be8c1d4157a145772d4325443139f
Mike <mnightingale@tutanota.com>
2021-01-09 01:29:50 +0000

    Add TokenDocument as model export

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,3 @@
-export { User, isUser, TokenObject, UserDocument } from "./User";
+export { User, isUser, TokenObject, UserDocument, TokenDocument } from "./User";
 export { Movie } from "./Movie";
 export { List, ListDocument, MovieIdDocument } from "./List";
