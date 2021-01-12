77960f74a5d67903e7693327fbd0bac7817fe2aa
Mike <mnightingale@tutanota.com>
2021-01-12 19:16:06 +0000

    Add logging to file "log.json"

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,20 @@
+import { createWriteStream } from "fs";
+import { join } from "path";
 import bunyan from "bunyan";
 
 export const logger = bunyan.createLogger({
   name: "Movie Organiser",
+  streams: [
+    {
+      level: "info",
+      stream: process.stdout,
+    },
+    {
+      type: "file",
+      level: "warn",
+      stream: createWriteStream(join(__dirname, "..", "log.json"), {
+        flags: "a",
+      }),
+    },
+  ],
 });
