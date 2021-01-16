214be10cbb2a4d6241e6cd5b87c20fece54ea613
Mike <mnightingale@tutanota.com>
2021-01-16 20:54:58 +0000

    Add CORS policy

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import passport from "passport";
+import cors from "cors";
 // @ts-ignore Catches async errors in express routes
 import "express-async-errors";
 
@@ -10,4 +11,9 @@ export const app = express();
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(passport.initialize());
+
+// TODO: Set CORS policy to only allow same origin
+app.options("*", cors<any>());
+app.use(cors());
+
 app.use(router);
