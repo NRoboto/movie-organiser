76f74cbe17000d3fce812cb43c8ae9f33dacc926
Mike <mnightingale@tutanota.com>
2021-01-16 18:42:13 +0000

    Client add init pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,15 +7,31 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { SigninPageComponent } from './pages/signin-page/signin-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { FriendsPageComponent } from './pages/friends-page/friends-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { ListsPageComponent } from './pages/lists-page/lists-page.component';
+import { SearchsPageComponent } from './pages/searchs-page/searchs-page.component';
+import { UserSettingsPageComponent } from './pages/user-settings-page/user-settings-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    SignupPageComponent,
     SigninPageComponent,
+    HomePageComponent,
     Error404PageComponent,
+    ProfilePageComponent,
+    FriendsPageComponent,
+    ListPageComponent,
+    ListsPageComponent,
+    SearchsPageComponent,
+    UserSettingsPageComponent,
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/pages/searchs-page/searchs-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchs-page',
+  templateUrl: './searchs-page.component.html',
+  styleUrls: ['./searchs-page.component.css']
+})
+export class SearchsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
