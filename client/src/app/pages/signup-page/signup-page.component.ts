7feda3122f343f5c43a74d9457df5674952e2ce8
Mike <mnightingale@tutanota.com>
2021-01-16 19:57:19 +0000

    Client add signup page design

--- client/src/app/pages/signup-page/signup-page.component.ts
+++ client/src/app/pages/signup-page/signup-page.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.css']
+  styleUrls: ['./signup-page.component.css'],
 })
 export class SignupPageComponent implements OnInit {
+  public genders = ['Female', 'Male', 'Non-binary', 'Other...'] as const;
 
-  constructor() { }
+  public signupInfo: {
+    username: string;
+    password: string;
+    age?: number;
+    gender?: string;
+    customGender?: string;
+    location?: string;
+  } = {
+    username: '',
+    password: '',
+  };
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  signupSubmit() {
+    console.log(this.signupInfo);
   }
-
 }
